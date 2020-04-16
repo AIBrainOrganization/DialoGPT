154f3fae2e5bca7c9e3417df967c9221aeb696ab
CheongAn Lee <calee@aibrain.co.kr>
2020-04-16 15:09:59 +0900

    Configuration file.

---
+++ config.py
@@ -0,0 +1,11 @@
+# device for forward and backward model
+device_f = 'cuda'
+device_r = 'cpu'
+
+# sampling parameters
+top_k = 50
+MMI_temperature = 0.5
+
+num_samples = 40
+ALPHA = 0.5
+top_p = 0.9
