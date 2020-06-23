43d2430a31e9f762744043253d7c1cb252c21871
CheongAn Lee <calee@aibrain.co.kr>
2020-06-23 09:37:07 +0900

    Dockerfile for training.

--- config.py
+++ config.py
@@ -14,11 +14,11 @@ min_p_alpha = 2
 
 # default paths
 vocab_path = 'models/kogpt2_news_wiki_ko_cased_818bfa919d.spiece'
-# model_path = 'models/output_model/' \
-#     'GPT2.1e-05.64.2gpu.2020-03-24160510/GP2-pretrain-step-20672.pkl'
-# reverse_model_path = 'models/output_model/' \
-#     'GPT2.1e-05.64.2gpu.2020-03-26162233/GP2-pretrain-step-8704.pkl'
-model_path = 'models/dcinside-news_new/' \
-    'GPT2.1e-05.64.2gpu.2020-05-19161142/GP2-pretrain-step-61920.pkl'
-reverse_model_path = 'models/dcinside-news_new/' \
-    'GPT2.1e-05.64.2gpu.2020-05-20084102/GP2-pretrain-step-61920.pkl'
+model_path = 'models/output_model/' \
+    'GPT2.1e-05.64.2gpu.2020-03-24160510/GP2-pretrain-step-20672.pkl'
+reverse_model_path = 'models/output_model/' \
+    'GPT2.1e-05.64.2gpu.2020-03-26162233/GP2-pretrain-step-8704.pkl'
+# model_path = 'models/dcinside-news_new/' \
+#     'GPT2.1e-05.64.2gpu.2020-05-19161142/GP2-pretrain-step-61920.pkl'
+# reverse_model_path = 'models/dcinside-news_new/' \
+#     'GPT2.1e-05.64.2gpu.2020-05-20084102/GP2-pretrain-step-61920.pkl'
