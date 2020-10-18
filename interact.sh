e1dc468e28a23c1c1d32e1f0b17bf29475331652
CheongAn Lee <calee@aibrain.co.kr>
2020-10-18 15:16:16 +0900

    interact.sh

---
+++ interact.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python interact.py models/kogpt2_news_wiki_ko_cased_818bfa919d.spiece models/output_model/GPT2.1e-05.64.2gpu.2020-03-24160510/GP2-pretrain-step-20672.pkl models/output_model/GPT2.1e-05.64.2gpu.2020-03-26162233/GP2-pretrain-step-8704.pkl
